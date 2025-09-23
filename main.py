6c4f937096bcc8576a26af4cc4225c082199b012
Naka Assoumatine <rassoumatine@mail.wlu.edu>
2025-09-23 14:58:50 -0400

    Add lines

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1, border=0)
-    pdf.line(10, 21, 200, 21)
 
     # Set the footer
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    # Add lines
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
@@ -30,5 +33,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        # Add lines
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
